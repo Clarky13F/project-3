c5db479339f33760cf004456e0a5b379556ed585
Brandon Clark <clarkbrandon141@gmail.com2666brandon clark$ git config --global user.email clarkbrandon141@gmail.com>
2024-01-30 21:06:46 -0600

    added starter code

---
+++ client/src/App.jsx
@@ -0,0 +1,48 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
+import { HelmetProvider } from "react-helmet-async";
+import { Outlet } from "react-router-dom";
+import Auth from "./components/Auth";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <Provider store={store}>
+          <Auth>
+            <Outlet />
+          </Auth>
+        </Provider>
+      </HelmetProvider>
+    </ApolloProvider>
+  );
+}
+
+export default App;
