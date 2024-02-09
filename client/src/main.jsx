480ff50a8de99882a8fa9a275d8fb6281ae6dc26
RPMcQuigg <142121973+RPMcQuigg@users.noreply.github.com>
2024-02-08 21:05:16 -0600

    forgot to save main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import "symbol-observable";
-
 import Home from "./pages/Home.jsx";
+import About from "./pages/About";
 import Page404 from "./pages/Page404.jsx";
 import Login from "./pages/Login.jsx";
 import SignUp from "./pages/SignUp.jsx";
@@ -13,27 +13,36 @@ import FoundItems from "./pages/founditems.jsx";
 //import Profile from "./pages/profile.jsx";
 import App from "./App.jsx";
 
+// defining pages, using createBrowserRouter from React router dom, with routes:
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    error: <Page404 />,
+    errorElement: <Page404 />,
     children: [
       {
         index: true,
         element: <Home />,
       },
       {
-        path: "/dashboard",
-        element: <Dashboard />,
+        path: "/About",
+        element: <About />
+      },
+      // {
+      //   path: "/profile",
+      //   element: <Profile />
+      // },
+      {
+        path: "/Login",
+        element: <Login />
       },
       {
-        path: "/login",
-        element: <Login />,
+        path: "/SignUp",
+        element: <SignUp />
       },
       {
-        path: "/signup",
-        element: <SignUp />,
+        path: "/Dashboard",
+        element: <Dashboard />
       },
       {
         path: "/lostitems",
